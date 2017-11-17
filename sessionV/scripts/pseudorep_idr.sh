eb48ef64a857261886f3c1e85a15cbafb96739b3
Chris Fields <cjfields@illinois.edu>
2017-11-17 00:18:58 -0600

    shore up

---
+++ sessionV/scripts/pseudorep_idr.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# USAGE: sh pseudorep_idr.sh <input BAM rep1> <chip BAM rep1> <input BAM rep2> <chip BAM rep2> <NAME for IDR output>
+
+# This script will take the BAM files and perform the following steps:
+    ## Merge BAMs for ChiP files,
+    ## Shuffle reads and split into two new BAM files (pseudo-replicates),
+    ## Merge BAMs for Input files,
+    ## Shuffle reads and split into two new BAM files (pseudo-replicates),
+    ## Call peaks on pseudo-replicates with MACS2 ,
+    ## Sort peaks called on pseudo-replicates,
+    ## IDR analysis using pseudo-replicate peak calls
+
+# Please use the following SLURM directives:
+	## -t 10:00
+	## -p classroom
+	## --mem=4000
+	## -e pseudorep-idr.err
+
+module load SAMtools/1.5-IGB-gcc-4.9.4
+module load MACS2/2.1.1.20160309-IGB-gcc-4.9.4-Python-2.7.13
+
+date
+
+inputFile1=`basename $1`
+treatFile1=`basename $2`
+inputFile2=`basename $3`
+treatFile2=`basename $4`
+EXPT=$5
+
+NAME1=`basename $treatFile1 _full.bam`
+NAME2=`basename $treatFile2 _full.bam`
+
+# Make Directories
+mkdir -p /scratch/mm573/idr_ngscourse/macs
+mkdir -p /scratch/mm573/idr_ngscourse/pooled_pseudoreps
+mkdir -p /scratch/mm573/idr_ngscourse/tmp
+
+# Set paths
+baseDir=/home/classroom/hpcbio/chip-seq/bowtie2/
+macsDir=/scratch/mm573/idr_ngscourse/macs
+outputDir=/scratch/mm573/idr_ngscourse/pooled_pseudoreps
+tmpDir=/scratch/mm573/idr_ngscourse/tmp
+
+#Merge treatment BAMS
+echo "Merging BAM files for pseudoreplicates..."
+samtools merge -u ${tmpDir}/${NAME1}_${NAME2}_merged.bam $baseDir/${treatFile1} $baseDir/${treatFile2}
+samtools view -H ${tmpDir}/${NAME1}_${NAME2}_merged.bam > ${tmpDir}/${EXPT}_header.sam
+
+#Split merged treatments
+nlines=$(samtools view ${tmpDir}/${NAME1}_${NAME2}_merged.bam | wc -l ) # Number of reads in the BAM file
+nlines=$(( (nlines + 1) / 2 )) # half that number
+
+# This will shuffle the lines in the file and split it into two SAM files
+samtools view ${tmpDir}/${NAME1}_${NAME2}_merged.bam | shuf - | split -d -l ${nlines} - "${tmpDir}/${EXPT}"
+
+cat ${tmpDir}/${EXPT}_header.sam ${tmpDir}/${EXPT}00 | samtools view -bS - > ${outputDir}/${EXPT}00.bam
+cat ${tmpDir}/${EXPT}_header.sam ${tmpDir}/${EXPT}01 | samtools view -bS - > ${outputDir}/${EXPT}01.bam
+
+#Merge input BAMS
+echo "Merging input BAM files for pseudoreplicates..."
+samtools merge -u ${tmpDir}/${NAME1}input_${NAME2}input_merged.bam $baseDir/${inputFile1} $baseDir/${inputFile2}
+
+#Split merged treatment BAM
+nlines=$(samtools view ${tmpDir}/${NAME1}input_${NAME2}input_merged.bam | wc -l ) # Number of reads in the BAM file
+nlines=$(( (nlines + 1) / 2 )) # half that number
+samtools view ${tmpDir}/${NAME1}input_${NAME2}input_merged.bam | shuf - | split -d -l ${nlines} - "${tmpDir}/${EXPT}_input" # This will shuffle the lines in the file and split in two
+cat ${tmpDir}/${EXPT}_header.sam ${tmpDir}/${EXPT}_input00 | samtools view -bS - > ${outputDir}/${EXPT}_input00.bam
+cat ${tmpDir}/${EXPT}_header.sam ${tmpDir}/${EXPT}_input01 | samtools view -bS - > ${outputDir}/${EXPT}_input01.bam
+
+#Peak calling on pseudoreplicates
+echo "Calling peaks for pseudoreplicate1 "
+macs2 callpeak -t ${outputDir}/${EXPT}00.bam -c ${outputDir}/${EXPT}_input00.bam -f BAM -g hs -n $macsDir/${NAME1}_pr -B -p 1e-3  2> $macsDir/${NAME1}_pr_macs2.log
+
+echo "Calling peaks for pseudoreplicate2"
+macs2 callpeak -t ${outputDir}/${EXPT}01.bam -c ${outputDir}/${EXPT}_input01.bam -f BAM -g hs -n $macsDir/${NAME2}_pr -B -p 1e-3  2> $macsDir/${NAME2}_pr_macs2.log
+
+#Sort peak by -log10(p-value)
+echo "Sorting peaks..."
+sort -k8,8nr $macsDir/${NAME1}_pr_peaks.narrowPeak | head -n 100000 > $macsDir/${NAME1}_pr_sorted.narrowPeak
+sort -k8,8nr $macsDir/${NAME2}_pr_peaks.narrowPeak | head -n 100000 > $macsDir/${NAME2}_pr_sorted.narrowPeak
+
+#Independent replicate IDR
+module purge
+module load IDR/2.0.4-IGB-gcc-4.9.4-Python-3.6.1
+echo "Running IDR on pseudoreplicates..."
+idr --samples $macsDir/${NAME1}_pr_sorted.narrowPeak $macsDir/${NAME2}_pr_sorted.narrowPeak --input-file-type narrowPeak --output-file ${EXPT}_pseudorep-idr --rank p.value --plot
+
+
+# Remove the tmp directory
+rm -r $tmpDir
